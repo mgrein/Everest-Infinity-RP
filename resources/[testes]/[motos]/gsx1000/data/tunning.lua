ef828b03c204d0d36aeb1169da27daad564e1aaf
Matheus Grein <xbox.matheus1202@hotmail.com>
2020-03-16 22:08:07 -0300

    Tirei a porra toda

--- resources/[testes]/[motos]/gsx1000/data/tunning.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function()
-	AddTextEntryByHash(GetHashKey("gsx_seat_1"),"Tuning 01")	
-		
-end)
-
---Yamaha MT-03=mt03
--- resources/[testes]/[motos]/zx10r/data/tunning.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-	AddTextEntryByHash(GetHashKey("ar_ex1"),"Tuning 01")	
-	AddTextEntryByHash(GetHashKey("black_mgrd_2"),"Tuning 02")	
-	AddTextEntryByHash(GetHashKey("carbon_mgrd_3"),"Tuning 03")	
-	AddTextEntryByHash(GetHashKey("sc_ex2"),"Tuning 04")	
-	AddTextEntryByHash(GetHashKey("zx10r_body1"),"Tuning 05")
-end)
-
---Yamaha MT-03=mt03
